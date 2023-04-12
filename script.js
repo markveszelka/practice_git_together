646a043f9f54ea88fbf1dfd899ed29c747d3598d
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2023-04-12 15:29:55 +0200

    add order element functions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Hellobello');
+
+
+function orderElement(data) {
+    return `<div class="order">
+            <button class="deleteOrder">x</button>
+            <p class="orderProdName">${data.name}</p>
+            <div>
+            <p class="cartQ">${data.quantity}m</input>
+            </div>
+            <p class="orderPrice">Price: ${data.price} HUF</p>
+            </div>`;
+  }
+  
